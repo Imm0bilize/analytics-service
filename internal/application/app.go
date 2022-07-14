bc5dc3e2472b96e625cdae233ca05351736ced54
Nikolay Vedernikov <nick.vedernikow@yandex.ru>
2022-07-14 16:36:56 +0300

    added creation of db

--- internal/application/app.go
+++ internal/application/app.go
@@ -1,11 +1,13 @@
 package application
 
 import (
+	"analytic-service/internal/adapters/database/postrgre"
 	v1 "analytic-service/internal/adapters/http/v1"
 	"analytic-service/internal/config"
 	"analytic-service/pkg/auth"
 	"analytic-service/pkg/httpServer"
 	"analytic-service/pkg/logging"
+	"context"
 	"log"
 	"os"
 	"os/signal"
@@ -15,13 +17,18 @@ import (
 func Run(cfg *config.Config) {
 	logger, err := logging.New(cfg.Logger.Level, cfg.Logger.TsFormat)
 	if err != nil {
-		log.Fatal("fatal")
+		log.Fatal(err)
 	}
 
 	// RestAPI
 	authService, err := auth.New(cfg)
 	if err != nil {
-		log.Fatal()
+		log.Fatal(err)
+	}
+
+	db, err := postrgre.New(cfg)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	handler := v1.CreateHandler(authService.ValidateTokenStub, logger.MiddlewareLogging)
@@ -38,4 +45,21 @@ func Run(cfg *config.Config) {
 	case err = <-restServer.Notify():
 		log.Fatalf("httpServer.Notify: %s", err.Error())
 	}
+
+	ctx, cancelFn := context.WithTimeout(
+		context.Background(),
+		cfg.AppParams.ShutdownTimeout,
+	)
+	defer cancelFn()
+
+	if err := restServer.Shutdown(ctx); err != nil {
+		logger.ErrorF("error during shutdown httpServer: %v", err)
+	}
+	if err := db.Shutdown(); err != nil {
+		logger.ErrorF("error during shutdown DB: %v", err)
+	}
+	if err := authService.Shutdown(); err != nil {
+		logger.Error("error while close connection with auth service")
+	}
+
 }
