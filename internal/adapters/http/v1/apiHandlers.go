6bd1057b211b043466443fe2e392f1560c15bc2b
Nikolay Vedernikov <nick.vedernikow@yandex.ru>
2022-07-15 10:37:46 +0300

    implemented rest api handlers

--- internal/adapters/http/v1/apiHandlers.go
+++ internal/adapters/http/v1/apiHandlers.go
@@ -1,17 +1,46 @@
 package v1
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
-func getNumAgreedTasks(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("not implemented"))
+func (h *Handler) getNumAgreedTasks(w http.ResponseWriter, r *http.Request) {
+	num, err := h.domain.GetNumAgreedTasks(r.Context())
+	if err != nil {
+		http.Error(w, "error when querying the database", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(num); err != nil {
+		http.Error(w, "error during encoding to json", http.StatusInternalServerError)
+		return
+	}
 }
 
-func getNumRejectedTasks(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("not implemented"))
+func (h *Handler) getNumRejectedTasks(w http.ResponseWriter, r *http.Request) {
+	num, err := h.domain.GetNumRejectedTasks(r.Context())
+	if err != nil {
+		http.Error(w, "error when querying the database", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(num); err != nil {
+		http.Error(w, "error during encoding to json", http.StatusInternalServerError)
+		return
+	}
 }
 
-func getTotalTime(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("not implemented"))
+func (h *Handler) getTotalTime(w http.ResponseWriter, r *http.Request) {
+	num, err := h.domain.GetTotalTime(r.Context())
+	if err != nil {
+		http.Error(w, "error when querying the database", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(num); err != nil {
+		http.Error(w, "error during encoding to json", http.StatusInternalServerError)
+		return
+	}
 }
