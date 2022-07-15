7b65d6882c462fafbea7aed0f42aa6c77612eb65
Nikolay Vedernikov <nick.vedernikow@yandex.ru>
2022-07-15 14:34:05 +0300

    implement grpc server

--- internal/ports/domain.go
+++ internal/ports/domain.go
@@ -10,3 +10,10 @@ type ClientDomain interface {
 	GetNumAgreedTasks(ctx context.Context) (*dto.NumAgreedTasksDTO, error)
 	GetNumRejectedTasks(ctx context.Context) (*dto.NumRejectedTaskDTO, error)
 }
+
+type ManagementServerDomain interface {
+	CreateTask(ctx context.Context, taskID string) error
+	UpdateTasksState(ctx context.Context, taskID string, state string) error
+	SetTimeStart(ctx context.Context, id string, login string, startTime string) error
+	SetTimeEnd(ctx context.Context, id string, login string, endTime string) error
+}
