07a327e16cb07c88192cf81e7de79246a3ef57b0
Nikolay Vedernikov <nick.vedernikow@yandex.ru>
2022-07-18 11:57:16 +0300

    completed work with db

--- internal/adapters/database/postrgre/db.go
+++ internal/adapters/database/postrgre/db.go
@@ -44,6 +44,8 @@ func (d *db) Shutdown() error {
 	return d.instance.Close()
 }
 
+// Query
+
 func (d *db) getNumRecordsByState(ctx context.Context, state string) (int, error) {
 	var count int
 	err := d.instance.QueryRowContext(ctx, "select count(*) from tasks_app.tasks_state where tasks_app.tasks_state.state=$1", state).Scan(&count)
@@ -81,4 +83,91 @@ func (d *db) GetNumRejectedTasks(ctx context.Context) (int, error) {
 		return 0, database.ErrCreateQuery
 	}
 	return count, nil
+}
+
+func closingTxIfError(err error, tx *sql.Tx) error {
+	if err != nil {
+		txErr := tx.Rollback()
+		if txErr != nil {
+			return fmt.Errorf("rollback execution error {%s} after request execution {%w}", txErr.Error(), err)
+		}
+		return fmt.Errorf("query execution error: %w", err)
+	}
+	return nil
+}
+
+// Commands
+
+func (d *db) CreateTask(ctx context.Context, taskID string) error {
+	tx, err := d.instance.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = d.instance.ExecContext(ctx, "insert into tasks_app.tasks_state (id) values ($1)", taskID)
+	if err := closingTxIfError(err, tx); err != nil {
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("error when committing a transaction {%s}", err.Error())
+	}
+	return nil
+}
+
+func (d *db) SetTimeStart(ctx context.Context, taskID, login, startTime, newState string) error {
+	tx, err := d.instance.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = d.instance.ExecContext(ctx, "insert into tasks_app.user_accept_time (task_id, email, time_start) values ($1, $2, $3)", taskID, login, startTime)
+	if err := closingTxIfError(err, tx); err != nil {
+		return err
+	}
+
+	if newState != "" {
+		_, err = d.instance.ExecContext(ctx, "update tasks_app.tasks_state set state=$1 where id=$2", newState, taskID)
+		if err := closingTxIfError(err, tx); err != nil {
+			return err
+		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("error when committing a transaction {%s}", err.Error())
+	}
+	return nil
+}
+
+func (d *db) SetTimeEnd(ctx context.Context, taskID, login, endTime, newState string) error {
+	tx, err := d.instance.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = d.instance.ExecContext(ctx, "update tasks_app.user_accept_time set time_end=$1 where task_id=$2 and email=$3", endTime, taskID, login)
+	if err := closingTxIfError(err, tx); err != nil {
+		return err
+	}
+
+	if newState == taskStateAccepted || newState == taskStateRejected {
+		_, err = d.instance.ExecContext(ctx, "update tasks_app.tasks_state set time_agreement=("+
+			"select sum(tasks_app.user_accept_time.time_end - tasks_app.user_accept_time.time_start) from tasks_app.user_accept_time where task_id=$1)"+
+			"where id=$2",
+			taskID, taskID,
+		)
+
+		if err := closingTxIfError(err, tx); err != nil {
+			return err
+		}
+
+		_, err = d.instance.ExecContext(ctx, "update tasks_app.tasks_state set state=$1 where id=$2", newState, taskID)
+		if err := closingTxIfError(err, tx); err != nil {
+			return err
+		}
+	}
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("error when committing a transaction {%s}", err.Error())
+	}
+	return nil
 }
