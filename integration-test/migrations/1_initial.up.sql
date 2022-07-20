79ab09e0352c6ab85854648d04f9dfecee505721
Nikolay Vedernikov <nick.vedernikow@yandex.ru>
2022-07-20 10:11:55 +0300

    added migrations

---
+++ integration-test/migrations/1_initial.up.sql
@@ -0,0 +1,22 @@
+create schema if not exists tasks_app;
+
+create type task_state AS ENUM ('created', 'processing', 'accepted', 'rejected');
+
+
+create table if not exists tasks_app.tasks_state
+(
+    id              varchar(24) primary key not null,
+    state           task_state default 'created',
+    time_agreement  interval
+);
+
+create table if not exists tasks_app.user_accept_time
+(
+    id              serial primary key,
+    task_id         varchar(24) not null references tasks_app.tasks_state(id),
+    email           varchar(100) not null,
+    time_start      timestamp,
+    time_end        timestamp
+);
+
+insert into tasks_app.tasks_state (id, state, time_agreement) values ('test_id', 'accepted', '01:00:00')
