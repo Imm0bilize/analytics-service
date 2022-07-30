c374ffd841e1e2423d68bd303d6d5b6aa5435f07
Nikolay Vedernikov <nick.vedernikow@yandex.ru>
2022-07-30 18:26:11 +0300

    added using kafka consumer in application

--- internal/application/app.go
+++ internal/application/app.go
@@ -2,16 +2,19 @@ package application
 
 import (
 	v1 "analytic-service/internal/adapters/http/v1"
-	"analytic-service/internal/adapters/repository"
+	"analytic-service/internal/adapters/repository/idempotencyKeyRepo"
+	"analytic-service/internal/adapters/repository/tasksRepo"
 	"analytic-service/internal/adapters/rpc"
 	"analytic-service/internal/config"
 	"analytic-service/internal/domain/service"
 	"analytic-service/pkg/auth"
 	"analytic-service/pkg/httpServer"
+	"analytic-service/pkg/kafka"
 	"analytic-service/pkg/logging"
 	"analytic-service/pkg/postgre"
 	"context"
 	"log"
+	"net"
 	"os"
 	"os/signal"
 	"syscall"
@@ -24,12 +27,6 @@ func Run(cfg *config.Config) {
 	}
 	logger.Debugf("logger was successfully created")
 
-	// ValidateToken
-	authService, err := auth.New(cfg.Auth.Host, cfg.Auth.Port)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	// Database
 	pg, err := postgre.New(
 		logger,
@@ -40,14 +37,21 @@ func Run(cfg *config.Config) {
 		log.Fatalf("error when creating connection to db: %s", err.Error())
 	}
 	logger.Debugf("connection to the database was successful")
-	repo := repository.NewPgRepo(pg)
+	repo := tasksRepo.New(pg)
 
+	// Domain
 	domainService := service.New(repo, logger)
+
+	// ValidateToken
+	authService, err := auth.New(cfg.Auth.Host, cfg.Auth.Port)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// Rest
 	handler := v1.CreateHandler(domainService)
 	restServer := httpServer.New(
-		handler.GetHttpHandler(authService.ValidateToken, logger.MiddlewareLogging),
+		handler.GetHttpHandler(authService.ValidateTokenStub, logger.MiddlewareLogging),
 		cfg.Http.Port,
 		cfg.Http.ReadTimeout,
 		cfg.Http.WriteTimeout,
@@ -55,18 +59,23 @@ func Run(cfg *config.Config) {
 	restServer.Run()
 	logger.Debugf("http server started successfully")
 
-	// grpc/broker
-	grpcServer, err := rpc.New(cfg, logger, domainService)
+	// idempotencyKeyValidator
+	idempotencyRepo := idempotencyKeyRepo.New(pg)
+
+	// Broker
+	consumerConnection, err := kafka.NewConsumerConnection(cfg.Kafka.ClientID, net.JoinHostPort(cfg.Kafka.Host, cfg.Kafka.Port))
 	if err != nil {
-		log.Fatalf("error when creating grpc server: %s", err.Error())
+		logger.Fatalf("Error connecting to kafka: %s", err.Error())
 	}
-	grpcServer.Run()
-	logger.Debugf("grpc server started successfully")
+	broker := rpc.NewKafkaConsumer(logger, domainService, idempotencyRepo, *consumerConnection, cfg.Kafka.Topic)
+	logger.Debug("connection to kafka successfully created")
+
+	interrupt := make(chan os.Signal, 1)
+	broker.Run(interrupt)
 
 	logger.Info("the service is ready to work")
 
 	// Shutdown
-	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
 
 	select {
@@ -74,8 +83,8 @@ func Run(cfg *config.Config) {
 		logger.Infof("an interrupt signal was received: %s", s.String())
 	case err = <-restServer.Notify():
 		logger.Fatalf("httpServer.Notify: %s", err.Error())
-	case err = <-grpcServer.Notify():
-		logger.Fatalf("grpcServer.Notify: %s", err.Error())
+	case err = <-broker.Notify():
+		logger.Fatalf("broker.Notify: %s", err.Error())
 	}
 
 	ctx, cancelFn := context.WithTimeout(
@@ -93,8 +102,8 @@ func Run(cfg *config.Config) {
 	if err := authService.Shutdown(); err != nil {
 		logger.Errorf("error while close connection with auth service: %s", err.Error())
 	}
-	if err := grpcServer.Shutdown(); err != nil {
-		logger.Errorf("error during shutdown gRPC server: %s", err.Error())
+	if err := broker.Shutdown(); err != nil {
+		logger.Errorf("error during closing connection with kafka: %s", err.Error())
 	}
 
 }
