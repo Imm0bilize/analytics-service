a6ef7a67a7ce6e6dadaeb70f7933e3ba43ff8861
Nikolay Vedernikov <nick.vedernikow@yandex.ru>
2022-07-30 15:49:01 +0300

    update configs for kafka

--- internal/config/config.go
+++ internal/config/config.go
@@ -38,6 +38,13 @@ type grpcCfg struct {
 	Port    string `yaml:"port"`
 }
 
+type kafkaCfg struct {
+	Host     string `yaml:"host"`
+	Port     string `yaml:"port"`
+	Topic    string `yaml:"topic"`
+	ClientID string `yaml:"client_id"`
+}
+
 type dbCfg struct {
 	IsNeedMigration    bool   `yaml:"is_need_migration"`
 	NAttemptsToConnect int    `yaml:"n_attempts_to_connect"`
@@ -54,6 +61,7 @@ type Config struct {
 	Auth      authCfg   `yaml:"auth_service"`
 	Db        dbCfg     `yaml:"database"`
 	Grpc      grpcCfg   `yaml:"grpc_server"`
+	Kafka     kafkaCfg  `yaml:"kafka"`
 	AppParams appCfg    `yaml:"application_params"`
 }
 
